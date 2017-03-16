46d38fac70fb8ab6925acebb24980ba079ea1fcb
Alexey Zabelin <zabelin.alex@gmail.com>
2017-03-16 00:51:11 -0400

    Rework date parsing to handle new types
    With the introduction of error-chain library I've run into several issues with Vec here. Instead of having the type <Vec<Result<NaiveDate, ParseError>>> which implemented necessary traits for using .collect(), I now have a type of <Vec<Result<NaiveDate>> which requires a different apprroach.
    
    Besides, the way I was trying to parse the date was not that good. There is no need to try every single format if one of them has matched already.

--- src/extract_date.rs
+++ src/extract_date.rs
@@ -11,33 +11,35 @@ use errors::*;
 // Some formats borrowed from https://github.com/amir/article-date-extractor
 static FMTS: &'static [&str] = &["/%Y/%m/%d/", "/%Y/%d/%m/", "%Y-%m-%d", "%B %e, %Y", "%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%SZ", "%B %k, %Y, %H:%M %p", "%Y-%m-%d %H:%M:%S.000000"];
 
+// Use lazy_static to ensure we only compile the regex once
+lazy_static! {
+    // Regex by Newspaper3k  - https://github.com/codelucas/newspaper/blob/master/newspaper/urls.py
+    static ref RE: Regex = Regex::new(r"([\./\-_]{0,1}(19|20)\d{2})[\./\-_]{0,1}(([0-3]{0,1}[0-9][\./\-_])|(\w{3,5}[\./\-_]))([0-3]{0,1}[0-9][\./\-]{0,1})?").unwrap();
+}
+
 // Parse the date, trying out each format
-pub fn parse_date(input: &str) -> Result<NaiveDate, ParseError> {
-    let parse_attempt: Vec<Result<NaiveDate,ParseError>> = FMTS.iter().map(|&fmt| NaiveDate::parse_from_str(input, fmt)).collect::<Vec<Result<NaiveDate, ParseError>>>();
-    try_filter_dates(parse_attempt)
+fn parse_date(input: &str) -> Result<NaiveDate> {
+    let mut result: Result<NaiveDate> = Err("None of the formats matched the date".into());
+
+    'outer: for fmt in FMTS {
+        if let Ok(v) = NaiveDate::parse_from_str(input, fmt) {
+            { result = Ok(v);
+              break 'outer; }
+        }
+    }
+
+    result
 }
 
-// Attempt to find a successful parse, if there are none, return the first error encountered
-fn try_filter_dates(vec: Vec<Result<NaiveDate, ParseError>>) -> Result<NaiveDate, ParseError> {
-    let orig_vec: Vec<Result<NaiveDate, ParseError>> = vec.clone();
-    let filtered: Vec<Result<NaiveDate, ParseError>> = vec.into_iter().filter(|&item| item.is_ok()).collect();
-    match filtered.first() {
-        Some(v) => v.to_owned(),
-        None => orig_vec.first().unwrap().to_owned(),
-    }
-}
+// Extract date from a URL
+fn extract_from_url(url: &str) -> Option<String> {
+    let mut date: Option<String> = None;
 
-// Extract date from a URL and pass it to parse_date
-fn extract_from_url(url: &str) -> Result<NaiveDate, ParseError> {
-    // Use lazy_static to ensure we only compile the regex once
-    lazy_static! {
-       // Regex by Newspaper3k  - https://github.com/codelucas/newspaper/blob/master/newspaper/urls.py
-       static ref RE: Regex = Regex::new(r"([\./\-_]{0,1}(19|20)\d{2})[\./\-_]{0,1}(([0-3]{0,1}[0-9][\./\-_])|(\w{3,5}[\./\-_]))([0-3]{0,1}[0-9][\./\-]{0,1})?").unwrap();
+    if let Some(val) = RE.find(url) {
+        date = Some(val.as_str().to_string());
     }
-    match RE.find(url) {
-        Some(v) => parse_date(v.as_str()),
-        None => parse_date(""),
-    }
+
+    date
 }
 
 fn extract_from_ldjson<'a>(html: &'a Document) -> Result<NaiveDate, ParseError> {
